bc6ffcd4ecca0df37d9e017a7311cb74827a544d
xuminrui <1940184774@qq.com>
2017-09-28 15:04:06 +0800

    test～

---
+++ MRSwiftPrograme/MRSwiftPrograme/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MRSwiftPrograme
+//
+//  Created by idolplay-60003D on 2017/9/28.
+//  Copyright © 2017年 idolplay-60003D. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
